e245f12aa7a3fe8fb6d1394321294281b41ce81c
ColemanMatthew-FS <MRColeman1@student.fullsail.edu>
2022-07-21 15:52:17 -0400

    Updated a comment

--- api/api.js
+++ api/api.js
@@ -1,6 +1,10 @@
 const axios = require("axios");
 require('dotenv').config();
 
+//this is a method to send a post request to a server
+//the result of that request is the req object from...
+//...router.post("/registration", on the backend...
+//...this is returned by getRegistration
 const getRegistration = async (data)=>{
     return await axios.post(process.env.service_url, data);
 }
